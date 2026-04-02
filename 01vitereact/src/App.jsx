3a14afccc49d9b656a1b4d9dbc29485b46267710
Inshal <inshalmudassar715@gmail.com>
2026-04-02 15:24:30 +0500

    Understand React Flow And Structure

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,16 @@
-
+import Chai from "./chai"
 
 
 function App() {
 
   return (
-    <h1>Chai aur React with vite | Inshal</h1>
+    // <h1>Chai aur React with vite | Inshal</h1>
+    // <Chai/>
+    <>
+    <h1>Hello</h1>
+    <p>My name is Inshal.</p>
+    <Chai/>
+    </>
   )
 }
 
