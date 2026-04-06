9dc7387f689fa56bea6df8b3de3a3170628c64ff
Inshal <inshalmudassar715@gmail.com>
2026-04-06 16:15:35 +0500

    props Understanding

--- 03_tailwindProps/src/App.jsx
+++ 03_tailwindProps/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import Card3 from './components/card'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1 className='bg-green-400 text-black p-4 rounded-b-2xl'>Tailwind Test</h1>
+    {/* <h1 className='bg-green-400 text-black p-4 rounded-b-2xl'>Tailwind Test</h1> */}
+    <Card3 ratting= "3.5" subject="Chemistry"/>
+    <Card3 ratting= "4.7" subject="Physics"/>
+    <Card3 ratting= "2.3" subject="Math"/>
     </>
   )
 }
