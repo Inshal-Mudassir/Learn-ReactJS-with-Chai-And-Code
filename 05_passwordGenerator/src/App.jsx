136879cb39ade6846411cbaa4d458716bfbed4dc
Inshal <inshalmudassar715@gmail.com>
2026-04-07 12:44:52 +0500

    passwordGenerator Project Setup

---
+++ 05_passwordGenerator/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [characterAllowed, setCharacterAllowed] = useState(false)
+  const [password, setPassword] = useState("")
+
+  return (
+    <h1 className='text-4xl text-center text-white'>Password Genrator</h1>
+  )
+}
+
+export default App
